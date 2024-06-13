2357a7222f898f5615986f738eb8a7f28c11a83a
fasosl2 <faso.sl2@gmail.com>
2024-06-13 06:37:46 -0300

    first commit

---
+++ src/Conta.java
@@ -0,0 +1,33 @@
+public class Conta {
+
+    private int numero = 1;
+    private String agencia = "";
+    private String nomeCliente = "";
+    private double saldo = 0;
+    
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Conta conta = new Conta();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        conta.setAgencia(scanner.next());
+        System.out.println("Por favor, digite o número da Conta: ");
+        conta.setNumero(scanner.nextInt());
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        conta.setNomeCliente(scanner.next());
+        System.out.println("Por favor, digite o saldo: ");
+        conta.setSaldo(scanner.nextDouble());
+        scanner.close();
+
+        System.out.println("Olá " + conta.getNomeCliente()
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + conta.getAgencia() + ", conta "
+                + conta.getNumero() + " e seu saldo R$" + conta.getSaldo() + " já está disponível pra saque");
+        System.out.println("Número da Agência: " + conta.getAgencia());
+        System.out.println("Número da Conta: " + conta.getNumero());
+        System.out.println("Nome do Cliente: " + conta.getNomeCliente());
+        System.out.println("Saldo: R$" + conta.getSaldo());
+    }
+}
